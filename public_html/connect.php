200b819f668d6c037eb48387e4b4f6b5c0163786
Alexandra A <albu.alexandra33@gmail.com>
2018-12-29 22:02:11 +0200

    Login input works
    Now it works to add data for the login in the database.
    :D

--- public_html/connect.php
+++ public_html/connect.php
@@ -1,14 +1,39 @@
-<?php>
-$servername = "localhost:3306";
-$username = "root";
-$password = "password2";
-$database_name = "veggie";
-	  $conn= new mysqli($servername, $username, $password, $database_name);
-	  //2.Verificarea conexiunii
-	  if(!$conn)
-	  {
-		  die("Connection failed: ".mysqli_connect_error());
-	  }
-	  echo "Connected successfully";
+<?php
 
+    $password = "";
+    $name = $_POST['Name'];
+    $email = $_POST['Email'];
+    if ( isset ($_POST['password'] ) ) {
+        $password = $_POST['password'];
+        echo $password;
+    }
+
+    $username ="";
+    if ( isset ($_POST['username'] ) ) {
+        $username = $_POST['username'];
+    }
+
+    if ($username != "" && $password != "") {
+        $servername = "localhost";
+        $dbusername = "root";
+        $dbpassword = "";
+        $dbname = "veg_tr";
+
+        $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
+        if ($conn->connect_error) {
+            die('Connection crashed. It is dead. Bye.)' . $conn->connect_error);
+        } else {
+
+            $sql = "INSERT INTO mylogin (name_mylogin, username_mylogin, email_mylogin, pass_mylogin) VALUE ('{$name}', '{$username}', '{$email}', '{$password}')";
+
+            if ($conn->query($sql) === TRUE) {
+                echo "New record created successfully";
+            } else {
+                echo "Error: " . $sql . "<br>" . $conn->error;
+            }
+            header("Location: http://localhost:63343/VegFru/VegFru/public_html/index.html?_ijt=1rjlomr8619pi8garu20ucma3t");
+            echo "Connected";
+        }
+    }
 ?>
+
